2248650a1bf458fc3e72e1d1ba4d26f833192201
IgorJoaquimn <igor.joaquim.sc@gmail.com>
2025-01-03 20:39:57 +0000

    ci/cd fix

--- ml-job-run.sh
+++ ml-job-run.sh
@@ -1,3 +1,9 @@
 JOB_NAME="ml-job-$(date +%Y%m%d%H%M%S)"  # Generate the job name using the current timestamp
 sed "s/JOB_NAME_PLACEHOLDER/$JOB_NAME/" templates/ml-job-template.yaml > config_files/ml-job.yaml
-
+# Verify the output
+if [ -f "config_files/ml-job.yaml" ]; then
+    echo "ml-job.yaml created successfully:"
+    cat ml-job.yaml
+else
+    echo "Failed to create ml-job.yaml"
+fi
